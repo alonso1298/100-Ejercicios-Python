1a8801d2968bf2c5e269ec47ece41b07dfdf7849
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-12 16:46:21 -0600

    se agrega ejercicio

--- exercices-level-2/exercice-14.py
+++ exercices-level-2/exercice-14.py
@@ -1,2 +1,7 @@
 '''Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. 
 Después debe mostrar por pantalla la paga que le corresponde.'''
+
+pago_hora = 60
+horas_trabajo = int(input('¿Cuantas horas has trabajado?: '))
+
+print(f'El coste por hora trabajada es de ${pago_hora} has trabajado {horas_trabajo}, su total es de: ${pago_hora * horas_trabajo}')

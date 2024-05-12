27e85529727395f6b1de0946acb5370b3a1334c4
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-12 16:38:32 -0600

    Se agrega ejercicio

--- exercices-level-2/exercice-13.py
+++ exercices-level-2/exercice-13.py
@@ -1,3 +1,7 @@
 '''Escribir un programa que pregunte el nombre del usuario en la consola
  y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!,
  donde <nombre> es el nombre que el usuario haya introducido.'''
+ 
+nombre = input('Dime cual es tu nombre: ')
+
+print(f'!Hola {nombre}¡')

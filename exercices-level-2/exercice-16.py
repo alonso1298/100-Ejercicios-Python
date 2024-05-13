e2197525f1332ff8bac3b3892b32719f3c901cef
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-12 18:25:09 -0600

    Se agrega ejercicio

--- exercices-level-2/exercice-16.py
+++ exercices-level-2/exercice-16.py
@@ -2,3 +2,10 @@
 muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son 
 los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división
  entera respectivamente. '''
+ 
+num1 = int(input('Introduce el primer numero entero: '))
+num2 = int(input('Introduce el segundo numero entero: '))
+
+print(f'La division es {num1/num2}, su cociente es de {num1 % num2}')
+ 
+ 

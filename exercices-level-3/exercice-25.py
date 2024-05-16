f9148311aeff944d85fd2bed485763f97bf705c0
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-16 21:35:12 +0000

    Se agrega programa

--- exercices-level-3/exercice-25.py
+++ exercices-level-3/exercice-25.py
@@ -1,2 +1,6 @@
 '''Escribir un programa que pida al usuario que introduzca una 
 frase en la consola y muestre por pantalla la frase invertida.'''
+
+frase = input('Introduce una frase: ')
+invertida = frase[::-1]
+print(invertida)

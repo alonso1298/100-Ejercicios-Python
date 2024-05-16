1773297490d8699a68a3cb1c0ab1aa4f124753bb
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-16 21:04:37 +0000

    Se agrega programa

--- exercices-level-3/exercice-22.py
+++ exercices-level-3/exercice-22.py
@@ -1,4 +1,11 @@
 '''Escribir un programa que pregunte el nombre completo del usuario en la consola 
 y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, 
 otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula.
- El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera. '''
+El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera. '''
+
+nombre = input('Ingresa tu nombre completo:  ')
+print(nombre.lower())
+print(nombre.upper())
+#Cmabia la primera letra de cada String en mayuscula.
+print(nombre.title())
+

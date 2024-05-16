04c65590d220479d5cc2824f60a9396a89945202
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-16 21:20:39 +0000

    Se agrega programa

--- exercices-level-3/exercice-24.py
+++ exercices-level-3/exercice-24.py
@@ -1,4 +1,12 @@
 '''Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde 
-el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). 
+el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +52-913724710-56). 
 Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el
- número de teléfono sin el prefijo y la extensión.'''
+número de teléfono sin el prefijo y la extensión.'''
+
+telefono = input('Introduce un numero telefonico con el siguiente formato prefijo-número-extension: ')
+
+telefono = telefono.replace('+52-', '')
+telefono = telefono.replace('-', '')
+
+print(telefono)
+

43e8e8fd9ff240bf53edc33cb5ec578ff0375590
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-18 11:03:05 -0600

    Se agrega ejercicio

--- exercices-level-3/exercice-26.py
+++ exercices-level-3/exercice-26.py
@@ -1,3 +1,10 @@
 '''Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal,
  y después muestre por pantalla
  la misma frase pero con la vocal introducida en mayúscula'''
+ 
+frase = input('Introduce una frase: ').lower()
+vocal = input('Introduce una vocal: ').lower()
+
+frase = frase.replace(vocal, vocal.upper())
+
+print(frase)

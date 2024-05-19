723bd1c926109e7f8ee1d64770b13f96de85e90b
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-19 11:36:23 -0600

    Se agrega ejercicio

--- exercices-level-3/exercice-29.py
+++ exercices-level-3/exercice-29.py
@@ -2,3 +2,14 @@
 nacimiento en formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año.
  Adaptar el programa anterior para que también funcione cuando
  el día o el mes se introduzcan con un solo carácter.'''
+ 
+from datetime import datetime
+
+fecha_nacimiento_str = input('Ingresa tu fecha de nacimiento en formato dd/mm/aaaa: ')
+
+fecha_nacimiento = datetime.strptime(fecha_nacimiento_str, '%d/%m/%Y')
+
+print(f'El dia es: {fecha_nacimiento.day}')
+print(f'El mes es: {fecha_nacimiento.month}')
+print(f'El año es: {fecha_nacimiento.year}')
+

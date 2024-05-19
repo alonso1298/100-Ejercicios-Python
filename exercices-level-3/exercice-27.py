77a601d3441e43ce5cb042dbe3d4968ab5e6e41a
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-19 10:57:04 -0600

    Se agrega ejercicio

--- exercices-level-3/exercice-27.py
+++ exercices-level-3/exercice-27.py
@@ -1,3 +1,16 @@
 '''Escribir un programa que pregunte el correo electrónico del usuario en la consola y 
 muestre por pantalla otro correo electrónico con el mismo nombre 
 (la parte delante de la arroba @) pero con dominio ceu.es.'''
+
+correo = input('Introduce un correo electronico: ')
+
+arroba = correo.find('@')
+
+if arroba != -1:
+    
+    usuario = correo[0 : arroba]
+    nuevo_correo = usuario + '@ceu.es'
+    print(f'Su nuevo correo es: \n{nuevo_correo}')
+    
+else:
+    print('Correo no valido')

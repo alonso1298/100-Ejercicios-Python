ac5b9c03cee0adde82c9e400fa78b56c5332c006
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-19 11:46:00 -0600

    Se agrega ejercicio

--- exercices-level-3/exercice-30.py
+++ exercices-level-3/exercice-30.py
@@ -1,3 +1,9 @@
 '''Escribir un programa que pregunte por consola por los productos de una cesta de la compra,
  separados por comas, y muestre por pantalla
  cada uno de los productos en una línea distinta.'''
+ 
+productos = input('Ingresa los productos de la cesta de la compra separados por una coma: ')
+lista_productos = productos.split(',')
+
+for i in lista_productos:
+    print(i.strip())

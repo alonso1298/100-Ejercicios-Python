6cb6c7d68eeb887882c6967e56790d637e9edbf8
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-05-19 11:10:04 -0600

    Se agrega ejercicio

--- exercices-level-3/exercice-28.py
+++ exercices-level-3/exercice-28.py
@@ -1,3 +1,12 @@
 '''Escribir un programa que pregunte por consola el precio de un
- producto en euros con dos decimales y muestre por pantalla el número de euros y 
-el número de céntimos del precio introducido.'''
+ producto en pesos con dos decimales y muestre por pantalla el número de pesos y 
+el número de centavos del precio introducido.'''
+
+precio_producto = float(input('Ingresa el precio de un producto: '))
+
+pesos = int(precio_producto)
+centavos = precio_producto - pesos
+
+print(f'El precio en pesos es de: {pesos}')
+print(f'Los centavos son: {round(centavos,2)}')
+

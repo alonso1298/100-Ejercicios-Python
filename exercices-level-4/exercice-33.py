bb1c15605ef2d22f7450f6a8bc34025772599660
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-20 13:41:43 +0000

    Se agrega Ejercico

--- exercices-level-4/exercice-33.py
+++ exercices-level-4/exercice-33.py
@@ -1,3 +1,12 @@
 '''Escribir un programa que almacene la cadena de caracteres contraseña 
 en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña 
 introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas. '''
+
+password = 'S@bss2023.'.lower()
+
+usuario_password = input('Ingresa la contraseña: ')
+
+if usuario_password == password:
+    print(f'La contraseña es correcta')
+else:
+    print(f'La contraseña es incorrecta')

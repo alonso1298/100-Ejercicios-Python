76a856d3bfdcfd4af5a8aaaf0f0036ebb943d56b
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-20 13:47:01 +0000

    Se agrega Ejercico

--- exercices-level-4/exercice-34.py
+++ exercices-level-4/exercice-34.py
@@ -1,2 +1,11 @@
 '''Escribir un programa que pida al usuario dos números y muestre por pantalla su división. 
 Si el divisor es cero el programa debe mostrar un error. '''
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+try:
+    resultado = num1/num2
+    print(F'El resultado de la division es: {resultado}')
+except ZeroDivisionError:
+    print(f'No se puede dividir entre 0')

164c6d5d5fe932995510928f030356cb77567aca
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-20 13:36:54 +0000

    Se agrega Ejercico

--- exercices-level-4/exercice-32.py
+++ exercices-level-4/exercice-32.py
@@ -1,1 +1,8 @@
 '''Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.'''
+
+edad =  int(input('Ingresa tu edad: '))
+
+if edad >= 18:
+    print('Eres mayor de edad')
+else:
+    print('Eres menor de edad')

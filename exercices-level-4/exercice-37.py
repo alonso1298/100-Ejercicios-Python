a6b37321d8472db8113d8592dc59d3bce8ba615c
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-20 14:11:09 +0000

    Se agrega Ejercico

--- exercices-level-4/exercice-37.py
+++ exercices-level-4/exercice-37.py
@@ -2,3 +2,14 @@
 El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un nombre
  posterior a la N y el grupo B por el resto. Escribir un programa que pregunte al usuario su nombre y sexo,
   y muestre por pantalla el grupo que le corresponde.'''
+
+nombre = input('Ingrese su nombre:  ')
+sexo = input('Ingrese su sexo:  ')
+
+if sexo == 'M' and nombre < 'M':
+    print('El alumno pertenece al grupo A')
+elif sexo == 'M' and nombre > 'N':
+    print('El alumno pertenece al grupo B')
+elif sexo == 'F' and nombre < 'N':
+    print('El alumno pertenece al grupo B')
+

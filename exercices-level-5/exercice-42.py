07e9b07a12bf2458cb3de64cf0864889e1f8df9d
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-20 23:38:41 +0000

    Se agrega Ejercico

--- exercices-level-5/exercice-42.py
+++ exercices-level-5/exercice-42.py
@@ -1,2 +1,9 @@
 '''Escribir un programa que pida al usuario un número entero positivo
  y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas. '''
+
+
+numero = int(input('Ingresa un numero entero positivo: '))
+
+for i in range(1, numero + 1):
+    if numero % 2 != 0:
+        print(i, end=(', '))

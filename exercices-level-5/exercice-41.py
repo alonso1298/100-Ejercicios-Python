b3ea2776ea5dd13ad00fb10e5ef59028955e14af
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-20 23:17:00 +0000

    Se agrega Ejercico

--- exercices-level-5/exercice-41.py
+++ exercices-level-5/exercice-41.py
@@ -1,2 +1,7 @@
 '''Escribir un programa que pregunte al usuario su edad y muestre por 
 pantalla todos los años que ha cumplido (desde 1 hasta su edad).'''
+
+edad = int(input('Ingresa tu edad: '))
+
+for i in range(edad):
+    print(i + 1)

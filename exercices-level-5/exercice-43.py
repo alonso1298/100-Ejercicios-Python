147db738d57ba275ef8dd374ea476574bf5623fb
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-21 13:55:34 +0000

    Se agrega Ejercico

--- exercices-level-5/exercice-43.py
+++ exercices-level-5/exercice-43.py
@@ -1,2 +1,9 @@
 '''Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla
  la cuenta atrás desde ese número hasta cero separados por comas.'''
+
+numero = int(input('Ingresa un nuero entero positivo: '))
+
+while numero >= 0:
+    print(numero, end=', ')
+    numero -= 1
+    

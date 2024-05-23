d810c0f356c1697d3b81c7ec2e9b37c45d4fcbc6
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-23 20:48:21 +0000

    Se agrega Ejercico

--- exercices-level-5/exercice-45.py
+++ exercices-level-5/exercice-45.py
@@ -6,3 +6,8 @@ como el de más abajo, de altura el número introducido.
 ***
 ****
 ***** '''
+
+altura = int(input('Introduzca la altura del triángulo: '))
+
+for i in range(1, altura + 1):
+    print('*' * i)

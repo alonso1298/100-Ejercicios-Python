0d3756185d22cf844b83e6671f2cf4ac7796e9bd
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-05-23 15:25:30 +0000

    Se agrega Ejercico

--- exercices-level-5/exercice-44.py
+++ exercices-level-5/exercice-44.py
@@ -1,2 +1,17 @@
 '''Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años,
  y muestre por pantalla el capital obtenido en la inversión cada año que dura la inversión.'''
+
+cantidad = float(input('Ingresa la cantidad a invertir: '))
+interes = float(input('Ingresa el interes anual: '))
+anios = int(input('Ingresa el numero de años: '))
+capital = cantidad
+
+if anios != 0:
+    for i in range(1, anios + 1):
+        capital = capital * (1 + (interes / 100))
+        print(f'Capital en el año {i}: {round(capital, 2)}')
+else:
+    print('Ingresa un numero diferente de 0 para el numero de años')
+
+
+

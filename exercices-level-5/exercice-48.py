6c6f00f6f9da8aa71f2659251d9b305035e94a2b
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-04 15:07:14 +0000

    Se agrega Ejercico 48

--- exercices-level-5/exercice-48.py
+++ exercices-level-5/exercice-48.py
@@ -1,2 +1,12 @@
 '''Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
 pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.'''
+
+contraseniaUsuario = 'S@bss2023.'
+
+contrasenia = ''
+
+while contrasenia != contraseniaUsuario:
+    contrasenia = input('Introduce la contraseña: ')
+    if contrasenia == contraseniaUsuario:
+        print('Acceso concedido')
+        break

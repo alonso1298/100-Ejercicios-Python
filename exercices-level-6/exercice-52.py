4c3650dab1cbdd3a55ed35831eeb4e6a49400dc0
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-05 15:41:26 +0000

    Se agrega el ejercicio 52

--- exercices-level-6/exercice-52.py
+++ exercices-level-6/exercice-52.py
@@ -1,2 +1,9 @@
-''Escribir un programa que muestre el eco de todo lo que el usuario introduzca
+'''Escribir un programa que muestre el eco de todo lo que el usuario introduzca
  hasta que el usuario escriba “salir” que terminará.'''
+
+while True:
+    ingreso = input('Ingresa texto: ')
+    print(f'eco: {ingreso}')
+    salida = input('Presiona S para salir u otra tecla para continuar: ').lower()
+    if salida == 's':
+        break

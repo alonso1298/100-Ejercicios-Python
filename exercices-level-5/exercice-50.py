50645d06a1d8e8ceb1632290794a17d8d9e7cbc0
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-05 14:14:22 +0000

    Se agrega el ejercicio 50

--- exercices-level-5/exercice-50.py
+++ exercices-level-5/exercice-50.py
@@ -1,3 +1,10 @@
 '''Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una 
 las letras de la palabra introducida empezando por la última.
         '''
+
+palabra = input('Ingresa un palabra: ')
+palabra_invertida = palabra[::-1].replace(' ', '').lower()
+letras = list(palabra_invertida)
+
+print(letras)
+

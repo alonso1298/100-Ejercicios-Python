78c7acb24d336bd351269901721e64eb32843898
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-05 14:21:34 +0000

    Se agrega el ejercicio 51

--- exercices-level-6/exercice-51.py
+++ exercices-level-6/exercice-51.py
@@ -1,2 +1,9 @@
 '''Escribir un programa en el que se pregunte al usuario por una frase y una letra, 
 y muestre por pantalla el número de veces que aparece la letra en la frase.'''
+
+
+frase = input('Ingresa una frase: ')
+letra = input('Ingresa una letra: ')
+listaFrase = list(frase)
+letra_repeticion = listaFrase.count(letra)
+print(f'La letra "{letra}" se repite {letra_repeticion} veces en la frase')

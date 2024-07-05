351364201e55d24cb4c37fa3b1b549ceaf79d921
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-05 14:03:29 +0000

    Se agrega el ejercicio 49

--- exercices-level-5/exercice-49.py
+++ exercices-level-5/exercice-49.py
@@ -1,1 +1,13 @@
 '''Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no. '''
+
+num = int(input('Introduce un nuermo entero: '))
+
+def es_primo(num):
+    if num <= 1:
+        return 'No es primo'
+    for i in range(2, int(num  ** 0.5) + 1):
+        if num % i == 0:
+            return 'No es primo'
+    return 'Es es primo'
+
+print(es_primo(num))

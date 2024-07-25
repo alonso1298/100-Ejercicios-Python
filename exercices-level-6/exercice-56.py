7e8188b9e0e6890a27a5423e009a8f51565186ad
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-25 17:22:03 +0000

    Se agrega el ejercicio 56

--- exercices-level-6/exercice-56.py
+++ exercices-level-6/exercice-56.py
@@ -1,2 +1,9 @@
 '''Escribir un programa que pregunte al usuario los números ganadores de 
 la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor. '''
+
+numGnadores = input('Introduce los números ganadores de la loteria: ')
+
+listaNumeros = list(numGnadores)
+
+print(sorted(listaNumeros))
+

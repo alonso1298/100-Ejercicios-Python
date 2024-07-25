780a68285ee220218aea4c5189220a96646a3b67
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-25 17:39:49 +0000

    Se agrega el ejercicio 57

--- exercices-level-6/exercice-57.py
+++ exercices-level-6/exercice-57.py
@@ -1,2 +1,11 @@
 '''Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre 
 por pantalla en orden inverso separados por comas.'''
+
+numeros = []
+
+for i in range(1, 11):
+    numeros.append(i)
+
+numeros.sort(reverse=True)
+
+print(numeros)

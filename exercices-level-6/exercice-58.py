95c8ce34787bd5a6325d4b322f6e14cd5cfc87be
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-07-26 15:36:04 +0000

    Se agrega el ejercicio 58

--- exercices-level-6/exercice-58.py
+++ exercices-level-6/exercice-58.py
@@ -3,3 +3,15 @@
 pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista
  las asignaturas aprobadas. Al final el programa debe mostrar por pantalla las asignaturas
   que el usuario tiene que repetir.'''
+
+asignaturas = ['Matemáticas', 'Física', 'Química', 'Historia', 'Lengua']
+aisgnaturas_a_repetir =  []
+
+calificacion = 0
+
+for asignatura in asignaturas:
+    calificacion = int(input(f'Cuanto sacaste en las materia de {asignatura}: '))
+    if calificacion <= 5:
+        aisgnaturas_a_repetir.append(asignatura)
+    
+print(f'Deberas recursar las asignaturas: {aisgnaturas_a_repetir}')

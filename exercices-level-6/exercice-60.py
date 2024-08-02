6dd1401b532847c18416412914a3d7ae5446267a
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-08-02 16:31:46 +0000

    Se agrega ejercicio 60

--- exercices-level-6/exercice-60.py
+++ exercices-level-6/exercice-60.py
@@ -1,1 +1,11 @@
 '''Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo.'''
+
+palabra = input('Ingresa una palabra: ')
+
+palabraInvertida = palabra[::-1]
+
+if palabra == palabraInvertida:
+    print('Es palindromo')
+else:
+    print('No es palindromo')
+

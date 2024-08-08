12675121e505265689d27e98e8dff46ec55f5a98
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-08-08 15:31:39 +0000

    Se agrega ejercicio 63

--- exercices-level-7/exercice-63.py
+++ exercices-level-7/exercice-63.py
@@ -1,2 +1,9 @@
 '''Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y 
 muestre por pantalla su producto escalar.'''
+
+vector1 = [1,2,3]
+vector2 = [-1,0,2]
+
+producto_escalar = sum(i * j for i, j in zip(vector1, vector2))
+
+print(f'El producto esclar es: {producto_escalar}')

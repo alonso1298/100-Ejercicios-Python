81974d8b040443e75c045cd563f6af5006d6158d
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-08-22 18:49:48 +0000

    Se agrega ejercicio 68

--- exercices-level-7/exercice-68.py
+++ exercices-level-7/exercice-68.py
@@ -3,3 +3,17 @@ un curso {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después muestre por
 pantalla los créditos de cada asignatura en el formato <asignatura> tiene <créditos> créditos, 
 donde <asignatura> es cada una de las asignaturas del curso, y <créditos> son sus créditos. 
 Al final debe mostrar también el número total de créditos del curso.'''
+
+asignaturas = {
+    'Matemáticas': 6, 
+    'Física': 4, 
+    'Química': 5
+    }
+
+total = 0
+
+for clave, valor in asignaturas.items():
+    print(f'La asignatura {clave} tiene {valor} creditos. ')
+    total += valor
+    
+print(f'El total de los creditos es: {total}')

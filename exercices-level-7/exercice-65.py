0a672a6daa7e2f95fa5f958e653181ebb5a5ce4f
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-08-22 17:47:52 +0000

    Se agrega ejercicio 65

--- exercices-level-7/exercice-65.py
+++ exercices-level-7/exercice-65.py
@@ -1,3 +1,12 @@
 '''Escribir un programa que guarde en una variable el
  diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al 
  usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.'''
+
+monedas = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+
+divisa = input('Ingrese una divisa porfavor: ')
+
+if divisa in monedas:
+    print(f'El simbolo de la divisa es: {monedas[divisa]}')
+else: 
+    print('La divisa no esta en el diccionario')

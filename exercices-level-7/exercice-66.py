554eaa37c52da0587ede96cfbd8216ea6f1c71b1
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-08-22 17:56:47 +0000

    Se agrega ejercicio 66

--- exercices-level-7/exercice-66.py
+++ exercices-level-7/exercice-66.py
@@ -1,3 +1,17 @@
 '''Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y
  lo guarde en un diccionario. Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años,
   vive en <dirección> y su número de teléfono es <teléfono>.'''
+
+nombre = input('Intorduce tu nombre: ')
+edad = input('Intorduce tu edad: ')
+direccion = input('Intorduce tu direccion: ')
+telefono = input('Intorduce tu telefono: ')
+
+datos_usuario = {
+    'nombre': nombre, 
+    'edad': edad,
+    'direccion': direccion, 
+    'telefono': telefono
+}
+
+print(f'{datos_usuario["nombre"]} tienes una edad de: {datos_usuario["edad"]}, tu direccion es: {datos_usuario["direccion"]} y tu telefono es: {datos_usuario["telefono"]}')
